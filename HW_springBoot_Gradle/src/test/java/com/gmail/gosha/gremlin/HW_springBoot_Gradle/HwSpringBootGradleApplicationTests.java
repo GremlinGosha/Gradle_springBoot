e624cd51ddef10104f55629da9ab9eb322b89ce6
GremlinGosha <gremlin.gosha@gmail.com>
2022-07-21 18:58:37 +0300

    First commit
    First commit. HelloWorld

---
+++ HW_springBoot_Gradle/src/test/java/com/gmail/gosha/gremlin/HW_springBoot_Gradle/HwSpringBootGradleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gmail.gosha.gremlin.HW_springBoot_Gradle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HwSpringBootGradleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
